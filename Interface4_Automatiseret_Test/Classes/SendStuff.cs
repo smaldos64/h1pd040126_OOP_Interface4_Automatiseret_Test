82c4138be077119c3c74b7870d7af3820f5c0247
smaldos64 <gudumholm34@gmail.com>
2026-05-11 13:11:52 +0200

    Adderet ekstra klasse.

---
+++ Interface4_Automatiseret_Test/Classes/SendStuff.cs
@@ -0,0 +1,17 @@
+﻿using Interface4_Automatiseret_Test.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface4_Automatiseret_Test.Classes
+{
+    public class SendStuff : INotifier    
+    {
+        public void SendReceipt(string message)
+        {
+            Console.WriteLine($"Sender gå hjem besked til h1pd040126: {message}... (klasse SendStuff)");
+        }
+    }
+}
--- Interface4_Automatiseret_Test/Program.cs
+++ Interface4_Automatiseret_Test/Program.cs
@@ -9,13 +9,14 @@ namespace Interface4_Automatiseret_Test
             CreditCardProcessor creditCardProcessor_Object = new CreditCardProcessor();
             MobilePayProcessor mobilePayProcessor_Object = new MobilePayProcessor();
             EMailService eMailService_Object = new EMailService();
+            SendStuff sendStuff_Object = new SendStuff();
 
             /* Linjen herunder gør brug af den første Constructor i CheckoutManager, og den overholder 
              * derfor alle principperne i SOLID.
              * Vi sender en klasse der implementerer IPaymentProcessor (CreditCardProcessor) og
              * ingen klasse der implementerer INotifier, da denne parameter er optional. */
             CheckoutManager checkoutManager_Object = new CheckoutManager(creditCardProcessor_Object);
-
+            
             /* Linjen herunder gør brug af den anden Constructor i CheckoutManager, 
              * og den overholder derfor ikke alle principperne i SOLID.
              * Vi sender en klasse der implementerer IAdvancedPaymentProvider (MobilePayProcessor), 
@@ -26,18 +27,21 @@ namespace Interface4_Automatiseret_Test
              * og den overholder derfor alle principperne i SOLID.
              * Vi sender en klasse der implementerer IPaymentProcessor (MobilePayProcessor) 
              * og en klasse der implementerer INotifier (EMailService). */
-            CheckoutManager checkoutManager_Object2 = new CheckoutManager(mobilePayProcessor_Object, eMailService_Object);
+            CheckoutManager checkoutManager_Object2 = new CheckoutManager(creditCardProcessor_Object, eMailService_Object);
 
             // Linjen herunder giver compiler fejl, da CheckoutManager's constructor kræver en IPaymentProcessor,
             // og EMailService implementerer ikke dette interface.
             //CheckoutManager checkoutManager_Object3 = new CheckoutManager(eMailService_Object);
 
+            CheckoutManager checkoutManager_Object4 = new CheckoutManager(creditCardProcessor_Object, sendStuff_Object);
             Console.WriteLine();
             checkoutManager_Object.CompleteOrder(500);
             Console.WriteLine();
             checkoutManager_Object1.CompleteOrder(250);
             Console.WriteLine();
-            checkoutManager_Object2.CompleteOrder(1000);
+            checkoutManager_Object2.CompleteOrder(1500);
+            Console.WriteLine();
+            checkoutManager_Object4.CompleteOrder(2500);
 
             Console.ReadKey();
         }
